c3579e6b431d4ac1a71378edfa4c0294373ad3c0
Julian Feliciano <julsfelic@gmail.com>
2016-01-23 14:19:32 -0700

    Limit guessing to five guesses

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,36 +1,53 @@
 require "sinatra"
 require "sinatra/reloader"
 
+@@guesses = 5
+
 set :number, rand(100)
+set :message, ""
 set :color, "#FFF"
 
 def check_guess(guess)
   if guess > settings.number
     if (guess - settings.number) > 5
       settings.color = "red"
-      "Way too high!"
+      settings.message = "Way too high!"
     else
       settings.color = "#F77"
-      "Too high!"
+      settings.message = "Too high!"
     end
   elsif guess < settings.number
     if (settings.number - guess) > 5
       settings.color = "red"
-      "Way too low!"
+      settings.message = "Way too low!"
     else
       settings.color = "#F77"
-      "Too low!"
+      settings.message = "Too low!"
     end
   elsif guess == settings.number
     settings.color = "green"
-    "You got it right!"
+    settings.message = "You got it right!"
+  end
+end
+
+def check_guesses_remaining
+  @@guesses -= 1
+  if settings.color == "green"
+    settings.number = rand(100)
+    @@guesses = 5
+  elsif @@guesses == 0
+    settings.number = rand(100)
+    @@guesses = 5
+    settings.message = "You've lost! A new number has been generated."
   end
 end
 
 get '/' do
   guess = params["guess"].to_i
-  message = check_guess(guess)
-  erb :index, locals: { number: settings.number,
-                        message: message,
+  number = settings.number
+  check_guess(guess)
+  check_guesses_remaining
+  erb :index, locals: { number: number,
+                        message: settings.message,
                         color: settings.color }
 end
