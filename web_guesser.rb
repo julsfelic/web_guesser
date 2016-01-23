de9a89337c1fa6b98c11254c29fa386d3325bc51
Julian Feliciano <julsfelic@gmail.com>
2016-01-23 13:59:38 -0700

    Add background color according to guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,21 +2,27 @@ require "sinatra"
 require "sinatra/reloader"
 
 set :number, rand(100)
+set :color, "#FFF"
 
 def check_guess(guess)
   if guess > settings.number
     if (guess - settings.number) > 5
+      settings.color = "red"
       "Way too high!"
     else
+      settings.color = "#F77"
       "Too high!"
     end
   elsif guess < settings.number
     if (settings.number - guess) > 5
+      settings.color = "red"
       "Way too low!"
     else
+      settings.color = "#F77"
       "Too low!"
     end
   elsif guess == settings.number
+    settings.color = "green"
     "You got it right!"
   end
 end
@@ -24,5 +30,7 @@ end
 get '/' do
   guess = params["guess"].to_i
   message = check_guess(guess)
-  erb :index, locals: { number: settings.number, message: message }
+  erb :index, locals: { number: settings.number,
+                        message: message,
+                        color: settings.color }
 end
